969e78a79b664c328cf33c3d5cb6244a793b8238
muktakim <muktakimsekh08095@gmail.com>
2026-07-25 22:26:07 +0530

    CTA Form

---
+++ src/components/faq/FAQ.tsx
@@ -0,0 +1,12 @@
+import './faq.scss'
+function FAQ() {
+    return (
+        <>
+            <div className="container-lg">
+                <h1>FAQ Works</h1>
+            </div>
+        </>
+    );
+}
+
+export default FAQ;
--- src/pages/home/Index.tsx
+++ src/pages/home/Index.tsx
@@ -1,6 +1,8 @@
 import { Footprints, Zap } from "lucide-react";
 import "./home.scss";
 import Pricing from "../../components/pricing/Pricing";
+import FAQ from "../../components/faq/FAQ";
+import Logo from "/images/logo3.svg";
 function Home() {
   return (
     <>
@@ -346,13 +348,24 @@ function Home() {
         </div>
       </section>
 
+      <FAQ />
+
       <section className="cta">
         <div className="cta-wrapper">
-          <div className="row m-0 w-100 h-fill align-items-center g-0">
+          <video
+            src="https://framerusercontent.com/assets/G0NwzP4bivPvK55b3ubxNslUs.mp4"
+            poster="https://framerusercontent.com/images/ZJ6HLYoAxMXsbBJCnggXHSRug.jpg?width=1920&height=1080"
+            autoPlay
+            loop
+            muted
+            playsInline
+            preload="auto"
+          />
+          <div className="row m-0 w-100 h-fill align-items-center g-0 justify-content-between">
             <div className="col-md-7 pe-5">
               <div className="common-heading">
                 <h2 className="section-heading mb-2">Let’s talk.</h2>
-                <p className="mb-4">
+                <p className="mb-0">
                   Tell us about your project
                   <span>
                     —whether it’s a website,
@@ -361,6 +374,7 @@ function Home() {
                   </span>
                 </p>
               </div>
+              <hr className="my-4 pt-2 pb-1" />
               <div className="row g-3">
                 <div className="col-md-4">
                   <div className="card">
@@ -397,16 +411,37 @@ function Home() {
                 </div>
               </div>
             </div>
-            <div className="col-md-5 h-fill">
-              <div className="cta-contact h-fill flex-column justify-content-center p-4">
+            <div className="col-md-4 h-fill">
+              <div className="cta-contact h-fill">
+                <img src={Logo} width={36} className="d-block mb-2" alt="" />
+                <h3>Have a project <span>in mind?</span></h3>
                 <form action="" className="w-100">
-                  <div className="form-group">
+                  <div className="form-group mb-3">
+                    <label htmlFor="">Your name*</label>
                     <input
                       type="text"
                       className="form-control"
-                      placeholder="Your name"
+                      placeholder="John Doe"
                     />
                   </div>
+                  <div className="form-group mb-3">
+                    <label htmlFor="">E-mail*</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="hello@site.com"
+                    />
+                  </div>
+                  <div className="form-group mb-3">
+                    <label htmlFor="">Message</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Message"
+                    />
+                  </div>
+                  <button className="btn w-100 text-white">Send Message</button>
+                  <small className="opacity-75 d-block mt-3">By submitting, you agree to our <a href="">Terms</a> and <a href="">Privacy Policy</a>.</small>
                 </form>
               </div>
             </div>
